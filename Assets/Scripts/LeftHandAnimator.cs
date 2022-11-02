884aaf0d072c30833912e019cc21d694d1e15541
Andrei Iacob <andreiiacob15@gmail.com>
2022-11-02 17:59:14 +0200

    'kitchen'

---
+++ Assets/Scripts/LeftHandAnimator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftHandAnimator : MonoBehaviour
+{
+    private Animator handAnimator;
+    void Start()
+    {
+        handAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (handAnimator != null)
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                if( !handAnimator.GetCurrentAnimatorStateInfo(0).IsName("CloseHand"))
+                    handAnimator.SetTrigger("TrClose");
+            }
+            else if (Input.GetKeyUp(KeyCode.G))
+            {
+                handAnimator.SetTrigger("TrOpen");
+            }
+        }
+    }
+}
