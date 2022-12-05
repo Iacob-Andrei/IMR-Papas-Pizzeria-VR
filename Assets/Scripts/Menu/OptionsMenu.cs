54676be74981c61c83d9a98bdf1240ce0a47ed55
Ana Maria Mitrea <anamitrea27@gmail.com>
2022-12-05 17:56:25 +0200

    Update on Options Menu difficulty chooser

--- Assets/Scripts/Menu/OptionsMenu.cs
+++ Assets/Scripts/Menu/OptionsMenu.cs
@@ -5,9 +5,35 @@ using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
+    private int _difficultyIdx = 0;
+    
     public AudioMixer audioMixer;
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetDifficulty(int difficultyIndex)
+    {
+        switch (difficultyIndex)
+        {
+            case 0:
+                Debug.Log("Difficulty set on: EASY");
+                _difficultyIdx = difficultyIndex;
+                break;
+            case 1:
+                Debug.Log("Difficulty set on: MEDIUM");
+                _difficultyIdx = difficultyIndex;
+                break;
+            default:
+                Debug.Log("Difficulty set on: HARD");
+                _difficultyIdx = difficultyIndex;
+                break;
+        }
     }
 }
